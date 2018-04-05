ea346b544d93d16913f94a95b09756395bd3f09c
GiannisFT <34090363+GiannisFT@users.noreply.github.com>
2018-04-05 18:21:33 +0200

    Add files via upload

---
+++ BankSystem/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    public class Account
+    {
+        public int accountNumber;
+        public string accountType;
+        public string accountOwner;
+        public double accountBalance;
+        //public static List<double> vault = new List<double>();
+
+        public Account(int accountnumber, string accounttype, string accountowner, double accountbalance)
+        {
+            this.accountNumber = accountnumber;
+            this.accountType = accounttype;
+            this.accountOwner = accountowner;
+            this.accountBalance = accountbalance;
+            //vault.Add(accountBalance);
+        }
+
+        public Account()
+        {
+
+        }
+
+        public static Account Add()
+        {
+            var accountDetails = new Account();
+            Console.WriteLine("Enter account number: ");
+            accountDetails.accountNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter account type: ");
+            accountDetails.accountType = Console.ReadLine();
+            Console.WriteLine("Enter account owner: ");
+            accountDetails.accountOwner = Console.ReadLine();
+            Console.WriteLine("Enter account balance: ");
+            accountDetails.accountBalance = double.Parse(Console.ReadLine());
+            return accountDetails;
+        }
+
+        public static void BankVault()
+        {
+            
+        }
+    }
+}
---
+++ BankSystem/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BankSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountlist = new List<Account>();
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("[1] Add account \n[6] Show accounts \n[7] Close account \n[8] Show bank vault \n[X] Close the program");
+                Console.Write("\nChoose an option: ");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        Console.Clear();
+                        accountlist.Add(Account.Add());
+                        break;
+                    case "6":
+                        Console.Clear();
+                        foreach (Account acc in accountlist)
+                        {
+                            Console.Write("{0}\t{1}\t{2}\t{3}", acc.accountNumber, acc.accountType, acc.accountOwner, acc.accountBalance);
+                            Console.WriteLine("");
+                        }
+                        Console.WriteLine("");
+                        break;
+                    case "7":
+                        Console.Clear();
+                        Console.Write("Enter account number: ");
+                        int aNum = int.Parse(Console.ReadLine());
+                        accountlist.RemoveAll(r => r.accountNumber == aNum);
+                        Console.WriteLine("");
+                        break;
+                    case "8":
+                        Console.Clear();
+                        List<double> blist = new List<double>();
+                        foreach (var item in accountlist)
+                        {
+                            blist.Add(item.accountBalance);
+                        }
+                        double vault = blist.Sum();
+                        Console.WriteLine(vault+"\n");
+                        break;
+                    case "x":
+                    case "X":
+                        exit = true;
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
